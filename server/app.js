d31d3f6301ced09c29eea27ec03f0854c9118a3a
Jason Spidle <jason.spidle@gmail.com>
2015-10-07 16:18:41 -0500

    Add comments to app.js

--- server/app.js
+++ server/app.js
@@ -1,27 +1,53 @@
+// Express init
 var express = require('express');
+var app = express();
+
+// other packages
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+// Node environment variables and configration
+var dotenv = require('dotenv').load();
+var config = require('./config/env/development');
+
+// server configration
+var port = process.env.PORT || config.port;
+
+// require routes
+var index = require('./routes/index');
+
+// require APIs
 var users = require('./routes/users');
 
-var app = express();
-
 // view engine setup
+// TODO update gulpfile to compile Jade files
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// serve favicon
 app.use(favicon(path.join(__dirname, '..', 'client', 'favicon.png')));
+
+// log stuff
 app.use(logger('dev'));
+
+// parse req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
 
-app.use('/', routes);
+// parse cookies
+app.use(cookieParser());
+
+// serve assets
+app.use(express.static(path.join(__dirname, '..', 'client', 'assets')));
+
+
+// use routes
+app.use('/', index);
+
+// use APIs
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -30,8 +56,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-// error handlers
 
 // development error handler
 // will print stacktrace
---
+++ server/config/env/development.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    app: {
+        name: 'Interviewly'
+    },
+    hostname: 'http://localhost:3000',
+    port: '3000',
+    debug: true,
+    logging: {
+        format: 'common'
+    }
+};
