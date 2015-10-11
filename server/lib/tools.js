008570a1a290949c5040912e68b874135e6955b5
Brendan Telzrow <telzrowb@gmail.com>
2015-10-11 16:28:33 -0500

    hard coded interview scheduler finished

--- server/lib/tools.js
+++ server/lib/tools.js
@@ -3,10 +3,14 @@ var shuffle = require('./modules/shuffle');
 var slots = require('./modules/getInterviewSlots');
 var sortByNum = require('./modules/sortByNum');
 var getCombinations = require('./modules/getCombinations');
+var scheduler = require('./modules/schedule');
 
 var Tools = {
     combine: function(interviewers, students, cb){
         cb(getCombinations.generate(interviewers,students));
+    },
+    schedule: function(interviewSlots, interviewers, students, combinations, interviewMax, companyMax, cb){
+        cb(scheduler.match(interviewSlots, interviewers, students, combinations, interviewMax, companyMax));
     },
     random: function(min, max, cb){
         cb(getRandom.between(min, max));
