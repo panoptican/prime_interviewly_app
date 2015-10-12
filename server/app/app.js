61c79f78071a582541d5bc2185c8e87ae88076ac
vleckas <vas.leckas@gmail.com>
2015-10-12 12:41:08 -0500

    updates

--- server/app/app.js
+++ server/app/app.js
@@ -1,15 +1,1 @@
-(function() {
-    'use strict';
-
-    // Declare app level module which depends on views and components
-    angular.module('interviewly', [
-        'ngRoute',
-        'interviewly.events',
-        'interviewly.interviewers',
-        'interviewly.students'
-    ]).
-        config(['$routeProvider', function($routeProvider) {
-            $routeProvider.otherwise({redirectTo: '/'});
-        }]);
-
-})();
+var app = angular.module('app', ['ngMaterial']);
