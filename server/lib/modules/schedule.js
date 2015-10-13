9ae3abf87f2c4ba520f9207d109f571dcba8640c
Brendan Telzrow <telzrowb@gmail.com>
2015-10-13 17:22:50 -0500

    added recursive schedule generation based on checking constraints

--- server/lib/modules/schedule.js
+++ server/lib/modules/schedule.js
@@ -1,6 +1,8 @@
 var shuffle = require('./shuffle');
 var sort = require('./sortByNum');
 
+var counter = 1;
+
 var scheduler = {
     book: function(student, interviewer, match){
         student.scheduled.count.total = 1 + (student.scheduled.count.total || 0);
@@ -33,6 +35,11 @@ var scheduler = {
     },
     //matches interviews
     match: function(interviewSlots, interviewers, students, combinations, interviewMax, companyMax){
+
+        var initCombinations = combinations.slice(),
+            initInterviewers = interviewers.slice(),
+            initStudents = students.slice();
+
         var currentStudents = shuffle.get(students),
             interviewers = shuffle.get(interviewers),
             sortedCombinations = sort.high(combinations),
@@ -45,8 +52,7 @@ var scheduler = {
             interviewers.forEach(function(interviewer, i){
                 sortedCombinations.some(function(interview, k){
 
-                    var cycler = (i + shifter) % students.length;
-                    var student = currentStudents[cycler];
+                    var student = currentStudents[(i + shifter) % students.length];
                     var lastCompany;
 
                     //checks to see the last company that student interviewed with
@@ -57,26 +63,26 @@ var scheduler = {
                         return lastCompany;
                     });
 
-                    //if interview ID matches the current interviewer AND interview student matches current student AND interview is available AND student has less than max interviews AND student has not interviewed with this person before AND the last interview was not with this company
-                    if(interview.interviewerID == interviewers[i].id && interview.student == student.name && interview.unavailable['slot' + s] == undefined && student.scheduled.count.total < interviewMax && student.scheduled.with[interview.interviewerID] == undefined && interview.company !== lastCompany){
 
+                    //if interview ID matches the current interviewer AND interview student matches current student AND interview is available AND student has less than max interviews AND student has not interviewed with this person before AND the last interview was not with this company
+                if( interview.interviewerID == interviewer.id &&
+                    interview.student == student.name &&
+                    interview.unavailable['slot' + s] == undefined &&
+                    student.scheduled.count.total < interviewMax &&
+                    student.scheduled.with[interview.interviewerID] == undefined
+                    //&& interview.company !== lastCompany
+                     ){
                         var match = interview;
                         match.slot = s;
 
                         //if student has no previous matches with this company, book interview
                         if(student.scheduled.count[interview.company] == undefined){
-                            Book(student, interviewer, match, schedule);
-                            sortedCombinations.splice(k, 1);
-                            schedule.push(match);
-                            return true;
-                        }
-                        //if last slot and student has s-3 number of interviews you must book an interview for this student
-                        else if(s > interviewSlots - 3 && student.scheduled.count.total < s-3 && student.scheduled[interview.company] < companyMax){
                             Book(student, interviewer, match);
                             sortedCombinations.splice(k, 1);
                             schedule.push(match);
                             return true;
-                        } //if student has no breaks AND interviewer has no breaks AND interviewer is not single
+                        }
+                        //if student has no breaks AND interviewer has no breaks AND interviewer is not single
                         else if(student.scheduled.count.break == undefined && interviewer.breaks < 1 && interviewer.single == false){
                             var match = {
                                 name: interviewer.name,
@@ -85,24 +91,27 @@ var scheduler = {
                                 interviewerID: interviewer.id,
                                 slot: s
                             };
-                            scheduleBreak(student, interviewer);
-                            schedule.push(match);
+                            scheduleBreak(student, interviewer, s);
+                            sortedCombinations.splice(k, 1);
+                            schedule.push(interview);
                             return true;
-                        } //if student has less than companyMax with this company, book interview
+                        }
+                        //if student has less than companyMax with this company, book interview
                         else if(student.scheduled.count[interview.company] < companyMax){
                             Book(student, interviewer, match);
                             sortedCombinations.splice(k, 1);
                             schedule.push(match);
                             return true;
-                        } else {
+                        }
+                        else {
                             var match = {
                                 name: interviewer.name,
                                 company: interviewer.company,
-                                student: "Break",
+                                student: "Break - No Match",
                                 interviewerID: interviewer.id,
                                 slot: s
                             };
-                            scheduleBreak(student, interviewer);
+                            scheduleBreak(student, interviewer, s);
                             schedule.push(match);
                             return true;
                         }
@@ -111,10 +120,25 @@ var scheduler = {
             });
             shifter++;
         }
-        var check = scheduler.check(students, interviewMax);
-        console.log(check);
-        var data = {schedule: schedule, students: students, interviewer: interviewers};
-        return data;
+
+
+
+        if(scheduler.check(students, interviewMax)){
+            return {schedule: schedule, students: students, interviewer: interviewers};
+        } else {
+            initStudents.forEach(function(student){
+                student.scheduled.with = {};
+                student.scheduled.count = {total: 0};
+            });
+            initInterviewers.forEach(function(interviewer){
+                interviewer.scheduled = {};
+                interviewer.breaks = 0;
+            });
+            counter++;
+            console.log(counter);
+            return this.match(interviewSlots, initInterviewers, initStudents, initCombinations, interviewMax, companyMax);
+        }
+
 
         function Book(student, interviewer, match){
             student.scheduled.count.total = 1 + (student.scheduled.count.total || 0);
@@ -123,7 +147,7 @@ var scheduler = {
             interviewer.scheduled['slot' + s] = student.name;
         }
 
-        function scheduleBreak(student, interviewer){
+        function scheduleBreak(student, interviewer, s){
             student.scheduled.count.break = 1 + (student.scheduled.count.break || 0);
             interviewer.scheduled['slot' + s] = "Break";
             interviewer.breaks += 1;
