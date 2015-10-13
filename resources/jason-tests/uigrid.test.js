233efdd3d7f407384424ad65bc2f32511d045610
Jason Spidle <jason.spidle@gmail.com>
2015-10-13 16:46:52 -0500

    Continuing adventures in ui-grid.

--- resources/jason-tests/uigrid.test.js
+++ resources/jason-tests/uigrid.test.js
@@ -2,21 +2,24 @@ var app = angular.module('app', ['ui.grid', 'ui.grid.edit']);
 
 app.controller('generateCtrl', ['$scope', '$http', function($scope, $http) {
 
-    var gridDest = document.getElementById('gridDest');
+    var gridDest = document.getElementById('grid-dest');
+    var gridCols = [];
+    var gridData = [];
+    var gridRows = [];
 
     $scope.generate = function() {
         $http({
             method: 'GET',
             url: 'http://localhost:3000/test'
         }).then(function successCallback(response) {
-            console.log(response.data);
-            angular.element(gridDest).empty().append('<grid-elem></grid-elem>');
-        }, function errorCallback(response) {});
-    }
-}]);
+            response.data.interviewer.forEach(function (item) {
+                gridCols.push(item.company);
+            });
 
-app.directive('grid-elem', ['$scope', function($scope) {
-    return {
-        template: '<div ui-grid="" class="myGrid"></div>'
+            $scope.gridOptions = {
+                
+            }
+
+        }, function errorCallback(response) {});
     }
 }]);
