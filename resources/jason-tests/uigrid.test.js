936095db232577eb5fa079abd94cf3bbfccbdd1d
Jason Spidle <jason.spidle@gmail.com>
2015-10-14 11:50:03 -0500

    Data display in UI Grid. Need better formatting still.

--- resources/jason-tests/uigrid.test.js
+++ resources/jason-tests/uigrid.test.js
@@ -7,15 +7,41 @@ app.controller('generateCtrl', ['$scope', '$http', function($scope, $http) {
     var gridData = [];
     var gridRows = [];
 
+    $scope.gridOptions = {
+        enableSorting: false,
+        columnDefs: gridCols,
+        data: gridData
+    };
+
     $scope.generate = function() {
         $http({
             method: 'GET',
             url: 'http://localhost:3000/test'
-        }).then(function successCallback(response) {
-            response.data.interviewer.forEach(function (item) {
-                gridCols.push(item.company);
-            });
+        })
+            .then(
+                function successCallback(response) {
+                    console.log(response);
+                    response.data.interviewer.forEach(function(item, pos) {
+                        gridCols.push({name: item.company + ' / ' + item.name, field: item.company + '_' + pos});
+                        //item.scheduled.forEach(function(item, pos) {
+                        //  console.log(item);
+                        //})
+                        var sched = item.scheduled;
+                        var students = [];
+                        Object.getOwnPropertyNames(sched).forEach(function (item, pos, array) {
+                            students.push(sched[item]);
+                        });
+                        gridData.push({[item.company + '_' + pos]: students});
+                    });
+                console.log(gridData);
+                },
 
-        }, function errorCallback(response) {});
+                function errorCallback(response) {}
+            )
+
+            .then(function () {
+                $scope.gridOptions.columnDefs = gridCols;
+                $scope.gridOptions.data = gridData;
+            })
     }
 }]);
