8241f24086bd1b34a0d1ab0d2be0c430f124e58a
Jason Spidle <jason.spidle@gmail.com>
2015-10-15 11:05:39 -0500

    UI grid generator in resources.

--- resources/jason-tests/uigrid.test.js
+++ resources/jason-tests/uigrid.test.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ui.grid', 'ui.grid.edit']);
+var app = angular.module('app', ['ui.grid', 'ui.grid.edit', 'ui.grid.rowEdit']);
 
 app.controller('generateCtrl', ['$scope', '$http', function($scope, $http) {
 
@@ -9,7 +9,10 @@ app.controller('generateCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.gridOptions = {
         enableSorting: false,
         columnDefs: gridCols,
-        data: gridData
+        data: gridData,
+        onRegisterApi: function (gridApi) {
+            $scope.gridApi = gridApi;
+        }
     };
 
     $scope.generate = function() {
@@ -17,42 +20,37 @@ app.controller('generateCtrl', ['$scope', '$http', function($scope, $http) {
             method: 'GET',
             url: 'http://localhost:3000/test'
         })
-            .then(
-                function successCallback(response) {
-                    console.log(response);
-                    gridCols = [];
-                    gridData = [];
-                    response.data.interviewer.forEach(function(item, pos) {
-                        gridCols.push({name: item.company + ' / ' + item.name, field: item.company + '_' + pos, width:200, displayName: item.company});
-                        var sched = item.scheduled;
-                        var students = [];
-                        Object.getOwnPropertyNames(sched).forEach(function (elem, index, array) {
-                            students.push({[item.company + '_' + pos]: sched[elem]});
-                        });
-                        gridData.push(students);
-                    });
-                console.log(gridData);
-                gridData = _.unzip(gridData);
-                console.log(gridData);
-                var gridArr = [];
-                gridData.forEach(function (item, pos) {
-                    var rowObj = {};
-                    item.forEach(function (elem, index) {
-                        _.extend(rowObj, elem);
-                    });
-                    gridArr.push(rowObj);
+        .then(function successCallback(response) {
+            console.log(response);
+            gridCols = [];
+            gridData = [];
+            response.data.interviewer.forEach(function(item, pos) {
+                gridCols.push({name: item.company + ' / ' + item.name, field: item.company + '_' + pos, width:150, displayName: item.company});
+                var sched = item.scheduled;
+                var students = [];
+                Object.getOwnPropertyNames(sched).forEach(function (elem, index, array) {
+                    students.push({[item.company + '_' + pos]: sched[elem]});
                 });
-                gridObj = {};
-                _.extend(gridObj, gridArr);
-                gridData = gridArr;
-                },
-
-                function errorCallback(response) {}
-            )
-
-            .then(function () {
-                $scope.gridOptions.columnDefs = gridCols;
-                $scope.gridOptions.data = gridData;
-            })
+                gridData.push(students);
+            });
+            console.log(gridData);
+            gridData = _.unzip(gridData);
+            console.log(gridData);
+            var gridArr = [];
+            gridData.forEach(function (item, pos) {
+                var rowObj = {};
+                item.forEach(function (elem, index) {
+                    _.extend(rowObj, elem);
+                });
+                gridArr.push(rowObj);
+            });
+            gridObj = {};
+            _.extend(gridObj, gridArr);
+            gridData = gridArr;
+            }
+        ).then(function () {
+            $scope.gridOptions.columnDefs = gridCols;
+            $scope.gridOptions.data = gridData;
+        })
     }
-}]);
+}])
