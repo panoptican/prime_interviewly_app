5573260534686b10d6b0d9aadc6e4b6fde78769e
Matthew Brown <MatthewBrown@Matthews-MacBook-Air.local>
2015-10-20 23:48:48 -0500

    all forms set up to pull in all form data into a function only need to set up http calls

--- server/app/views/partials/dialogs/interviewer/interviewer.controller.js
+++ server/app/views/partials/dialogs/interviewer/interviewer.controller.js
@@ -23,6 +23,17 @@ app.controller('interviewer', ['$scope', '$mdDialog', function($scope, $mdDialog
                 {name: 'career Delta'},
                 {name: 'mocks Epsilon'}
             ];
-        }
+        };
+        $scope.submit = function(fname, lname, title, company, link, desc){
+            var interviewer = {
+                fName: fname,
+                lName: lname,
+                title: title,
+                company: company,
+                link: link,
+                description: desc
+            };
+            console.log(interviewer);
+        };
     }
 }]);
