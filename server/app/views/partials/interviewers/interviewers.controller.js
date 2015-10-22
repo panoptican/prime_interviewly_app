b4f8c19b967d06319203a1fe09f83d9abb24cee7
Matthew Brown <MatthewBrown@Matthews-MacBook-Air.local>
2015-10-22 13:56:09 -0500

    interviewer table working but not good looking

--- server/app/views/partials/interviewers/interviewers.controller.js
+++ server/app/views/partials/interviewers/interviewers.controller.js
@@ -2,6 +2,7 @@ app.controller('interviewers', ['$scope', '$http', function($scope, $http){
  $scope.submit = function(){
   $http.get('/api/interviewer').then(function(response){
    console.log(response);
+   $scope.interviewers = response.data
   })
  }
 }]);
