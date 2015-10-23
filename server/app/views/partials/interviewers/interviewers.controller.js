5eff5cc8152ace74cbdd85d5d188f71f1d97e8c8
Matthew Brown <MatthewBrown@Matthews-MacBook-Air.local>
2015-10-23 15:29:44 -0500

    archive added for students and interviewers

--- server/app/views/partials/interviewers/interviewers.controller.js
+++ server/app/views/partials/interviewers/interviewers.controller.js
@@ -1,8 +1,16 @@
-app.controller('interviewers', ['$scope', '$http', function($scope, $http){
+app.controller('interviewers', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope){
   $http.get('/api/interviewer').then(function(response){
    console.log(response);
    $scope.interviewers = response.data
   });
+
+ $rootScope.$on('gotInterviewers', function(){
+  $http.get('/api/interviewer').then(function(response){
+   console.log(response);
+   $scope.interviewers = response.data
+  });
+ });
+
  $scope.editInterviewer = function(id) {
   $http.get('/api/interviewer?_id=' + id).then(function (response) {
    $scope.interviewer = response.data[0];
@@ -16,10 +24,15 @@ app.controller('interviewers', ['$scope', '$http', function($scope, $http){
     clickOutsideToClose: true
    })
   })
- }
+ };
+ $scope.archive = function(id){
+  $http.post('api/interviewer/archive?_id='+id, {archived: true}).then(function(response){
+   $rootScope.$broadcast('gotInterviewers');
+  })
+ };
 }]);
 
-app.controller('editInterviewer', ['$scope', '$mdDialog', 'items', function($scope, $mdDialog, items){
+app.controller('gotInterviewers', ['$scope', '$mdDialog', 'items', function($scope, $mdDialog, items){
  $scope.interviewer = items;
  console.log(items);
 
