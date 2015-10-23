b90b01b133a53c862d77ff74b094378b61af68db
Matthew Brown <MatthewBrown@Matthews-MacBook-Air.local>
2015-10-23 09:08:24 -0500

    interviwer edit dialog is working

--- server/app/views/partials/interviewers/interviewers.controller.js
+++ server/app/views/partials/interviewers/interviewers.controller.js
@@ -1,8 +1,30 @@
-app.controller('interviewers', ['$scope', '$http', function($scope, $http){
+app.controller('interviewers', ['$scope', '$http', '$mdDialog', function($scope, $http, $mdDialog){
  $scope.submit = function(){
   $http.get('/api/interviewer').then(function(response){
    console.log(response);
    $scope.interviewers = response.data
   })
+ };
+ $scope.editInterviewer = function(id) {
+  $http.get('/api/interviewer?_id=' + id).then(function (response) {
+   $scope.interviewer = response.data[0];
+   $mdDialog.show({
+    controller: 'editInterviewer',
+    locals: {
+     items: $scope.interviewer
+    },
+    templateUrl: 'views/partials/dialogs/interviewer/interviewerEdit.html',
+    parent: angular.element(document.body),
+    clickOutsideToClose: true
+   })
+  })
+ };
+}]);
+app.controller('editInterviewer', ['$scope', '$mdDialog', 'items', function($scope, $mdDialog, items){
+ $scope.interviewer = items;
+ console.log(items);
+
+ $scope.close = function(){
+  $mdDialog.hide();
  }
 }]);
