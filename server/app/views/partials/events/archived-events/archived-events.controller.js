75b6058456ee9944413b31d1f11c3cead933ad4d
Brendan Telzrow <telzrowb@gmail.com>
2015-10-24 17:20:42 -0500

    delete archived events working

--- server/app/views/partials/events/archived-events/archived-events.controller.js
+++ server/app/views/partials/events/archived-events/archived-events.controller.js
@@ -1,52 +1,36 @@
 /*
 Archived events controller
  */
-app.controller('archivedEventsCtrl', ['$scope', '$http', '$filter', function($scope, $http, $filter) {
-    $http.get('/api/event/archived').then(function success(response) {
+app.controller('archivedEventsCtrl', ['$scope', '$http', '$filter', '$rootScope', function($scope, $http, $filter, $rootScope) {
+    var archived = function(){
+        $http.get('/api/event/archived').then(function success(response) {
 
-        // set get request to a variable
-        eventsList = response.data;
+            // set get request to a variable
+            eventsList = response.data;
 
-        // initialize an empty array for storing data to be used in ng-repeat
-        var tiles = [];
+            // initialize an empty array for storing data to be used in ng-repeat
+            var tiles = [];
 
-        // iterate over response data and push objects into array for ng-repeat
-        eventsList.forEach(function(item, pos) {
-            tiles.push({title: title = item.cohort + ' ' + item.type, date: $filter('date')(new Date(item.date), 'MM/dd/yy'), _id: item._id});
-        });
+            // iterate over response data and push objects into array for ng-repeat
+            eventsList.forEach(function(item, pos) {
+                tiles.push({title: title = item.cohort + ' ' + item.type, date: $filter('date')(new Date(item.date), 'MM/dd/yy'), _id: item._id});
+            });
 
-        // set scope tiles equal to the object array
-        $scope.tiles = tiles;
+            // set scope tiles equal to the object array
+            $scope.tiles = tiles;
 
-    }, function error() {});
-
+        }, function error() {});
+    };
+    archived();
+    $rootScope.$on('got/archivedEvents', function(){
+        archived();
+    })
 }]);
 
-app.controller('modalCtrl', ['$scope', '$mdDialog', '$http', '$rootScope', function($scope, $mdDialog, $http, $rootScope) {
+app.controller('archivedEventsModal', ['$scope', '$mdDialog', '$http', '$rootScope', function($scope, $mdDialog, $http, $rootScope) {
     $scope.alert = '';
 
-    $scope.showAlert = function (id) {
-        // Appending dialog to document.body to cover sidenav in docs app
-        var confirm = $mdDialog.confirm()
-            .title('You are about to Archive this Event!')
-            .content('Click to confirm. You can access this Event at any time in Archived Events.')
-            .ariaLabel('Archive')
-            .ok('Got it!')
-            .cancel('Nevermind.');
-
-        $mdDialog.show(confirm).then(function () {
-            $http.post('api/event/archive?_id=' + id, {isArchived: true})
-                .then(function(response){
-                    $scope.status = 'You archived this event.';
-                    $rootScope.$broadcast('got/events');
-                });
-
-        }, function () {
-            $scope.status = 'You did not archive this event.';
-        });
-    };
-
-    $scope.showConfirm = function (id) {
+    $scope.deleteEvent = function (id) {
         // Appending dialog to document.body to cover sidenav in docs app
         var confirm = $mdDialog.confirm()
             .title('Would you like to delete this Event?')
@@ -59,7 +43,7 @@ app.controller('modalCtrl', ['$scope', '$mdDialog', '$http', '$rootScope', funct
             $http.delete('api/event?_id=' + id)
                 .then(function(response){
                     $scope.status = 'You deleted this event.';
-                    $rootScope.$broadcast('got/events');
+                    $rootScope.$broadcast('got/archivedEvents');
                 });
 
         }, function () {
