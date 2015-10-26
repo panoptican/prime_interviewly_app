49f16802937b37128e226bae447e0976bcfa3a62
Jason Spidle <jason.spidle@gmail.com>
2015-10-25 20:27:35 -0500

    Style adjustments to make app look good for presentation.

--- server/app/views/partials/events/schedule/schedule.controller.js
+++ server/app/views/partials/events/schedule/schedule.controller.js
@@ -4,23 +4,19 @@ Generate event controller
  */
 app.controller('eventSchedule', ['$scope', '$http', '$routeParams', 'scheduleConfig', 'eventDetails', function($scope, $http, $routeParams, scheduleConfig, eventDetails) {
 
-    // Save button
-    $scope.saveButton = 'Save';
-
     // event ID
     var eventParam = $routeParams._id;
-    $scope.eventId = eventParam;
 
-    // schedule data
-    var uglySchedule;
-    var scheduleId;
+    // initialize schedule variables
     var hasSchedule;
+    var scheduleId;
+    var uglySchedule;
 
-    // Initialize UI Grid variables
+    // initialize UI Grid variables
     var gridCols = [];
     var gridData = [];
 
-    // Initialize time slots column for UI Grid
+    // Initialize time slot columns for UI Grid
     var timeCol;
     var timeColLabel = {name: 'intTime', field: 'intTime', width: 100, pinnedLeft: true, enableCellEdit:false, displayName: 'Time Slot'};
 
@@ -59,33 +55,21 @@ app.controller('eventSchedule', ['$scope', '$http', '$routeParams', 'scheduleCon
             // set the unformatted schedule to uglySchedule
             uglySchedule = hasSchedule[0];
 
-            // Update the save button state
-            $scope.saveButton = 'Saved';
-
-            // empty the UI Grid variables in case there is data
-            $scope.gridOptions.columnDefs = [];
-            $scope.gridOptions.data = [];
-            gridCols = [];
-            gridData = [];
-
             // push time slot column into UI Grid variables
             gridCols.push(timeColLabel);
             gridData.push(timeCol);
 
             // format the schedule
-            var savedSchedule = scheduleConfig.formatSchedule(uglySchedule, gridCols, gridData);
+            var savedSched = scheduleConfig.formatSchedule(uglySchedule, gridCols, gridData);
 
             // set UI Grid values to saved schedule
-            $scope.gridOptions.columnDefs = savedSchedule.cols;
-            $scope.gridOptions.data = savedSchedule.data;
+            $scope.gridOptions.columnDefs = savedSched.cols;
+            $scope.gridOptions.data = savedSched.data;
         }
     });
 
     // "Generate" button click function
     $scope.generate = function () {
-
-        // Update the save button state
-        $scope.saveButton = 'Save';
 
         // empty the UI Grid variables in case there is data
         $scope.gridOptions.columnDefs = [];
@@ -106,19 +90,19 @@ app.controller('eventSchedule', ['$scope', '$http', '$routeParams', 'scheduleCon
             uglySchedule = response.data;
 
             // format the schedule
-            var savedSchedule = scheduleConfig.formatSchedule(uglySchedule, gridCols, gridData);
+            var genSched = scheduleConfig.formatSchedule(uglySchedule, gridCols, gridData);
 
             // set UI Grid values to saved schedule
-            $scope.gridOptions.columnDefs = savedSchedule.cols;
-            $scope.gridOptions.data = savedSchedule.data;
+            $scope.gridOptions.columnDefs = genSched.cols;
+            $scope.gridOptions.data = genSched.data;
+        }).then(function() {
+            $http.post('/api/event/saveSchedule?_id=' + eventParam, {_id: scheduleId}).then(function success(response) {
+                console.log(response);
+            })
         });
     };
 
     $scope.saveEvent = function() {
-        console.log(scheduleId);
-        $http.post('/api/event/saveSchedule?_id=' + eventParam, {_id: scheduleId}).then(function success(response) {
-            console.log(response);
-            $scope.saveButton = 'Saved';
-        })
+        console.log($scope.gridOptions.data);
     };
 }]);
