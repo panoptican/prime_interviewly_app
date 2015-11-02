2cf2dd1101b94bb176f3b2c72be49d83c3e0ccef
Brendan Telzrow <telzrowb@gmail.com>
2015-11-02 11:01:56 -0600

    Revert "removed console logs"
    This reverts commit 68ade34a095b5c47aedc41508224e38909b9697b.

--- server/models/users.js
+++ server/models/users.js
@@ -34,6 +34,7 @@ User.pre('save', function(next){
 });
 
 User.methods.comparePassword = function (candidatePassword, callback) {
+    console.log(candidatePassword);
     bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
         console.log(isMatch);
         if (err) return callback(err);
@@ -42,6 +43,7 @@ User.methods.comparePassword = function (candidatePassword, callback) {
 };
 
 User.statics.getAuthenticated = function (user, callback) {
+    console.log('getAuthenticated', user);
     this.findOne({username: user.username}, function (err, doc) {
         if (err) {
             console.log(err);
