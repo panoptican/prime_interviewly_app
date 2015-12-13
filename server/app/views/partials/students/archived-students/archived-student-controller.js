74a32d0f9b57bfe35e8b7244a90e5be35d94d2bf
Brendan Telzrow <telzrowb@gmail.com>
2015-12-13 09:53:27 -0600

    refactored archived students md-data-table

--- server/app/views/partials/students/archived-students/archived-student-controller.js
+++ server/app/views/partials/students/archived-students/archived-student-controller.js
@@ -1,9 +1,29 @@
-app.controller('archStudents', ['$scope', 'StudentFactory', function($scope, StudentFactory){
+app.controller('archStudents', ['$scope', '$mdToast', 'StudentFactory', function($scope, $mdToast, StudentFactory){
+    $scope.selected = [];
     $scope.students = StudentFactory.query({isArchived: true});
 
-    $scope.unarchive = function(id){
-        StudentFactory.update({id: id}, {isArchived: false}, function(response){
-            $scope.students = StudentFactory.query({isArchived: true});
-        });
+    $scope.cancelSelected = function() {
+        $scope.selected = [];
+    };
+
+    $scope.unArchiveSelected = function(selected){
+        var l = selected.length;
+        $mdToast.showSimple('Unarchived ' + l + ' students.');
+        while(l--){
+            var student = selected[l];
+            unarchive(student);
+            var i = $scope.selected.indexOf(student);
+            $scope.selected.splice(i, 1);
+        }
+    };
+
+    $scope.unarchive = function(student){
+        unarchive(student);
+    };
+
+    function unarchive(student){
+        StudentFactory.update({id: student._id}, {isArchived: false});
+        var i = $scope.students.indexOf(student);
+        $scope.students.splice(i, 1);
     }
 }]);
